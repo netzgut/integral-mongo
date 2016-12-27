fb95729836be7193f0ebc05d6829d8b86bb1107f
Benjamin Weidig <ben@netzgut.net>
2016-12-27 11:57:38 +0100

    Added FactoryDefaults

--- src/main/java/net/netzgut/integral/mongo/modules/MongoModule.java
+++ src/main/java/net/netzgut/integral/mongo/modules/MongoModule.java
@@ -1,13 +1,28 @@
 package net.netzgut.integral.mongo.modules;
 
+import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.services.FactoryDefaults;
+import org.apache.tapestry5.ioc.services.SymbolProvider;
 
+import net.netzgut.integral.mongo.configuration.MongoConfigurationSymbols;
 import net.netzgut.integral.mongo.internal.services.MongoServiceImplementation;
 import net.netzgut.integral.mongo.services.MongoService;
 
 public class MongoModule {
 
+    @FactoryDefaults
+    @Contribute(SymbolProvider.class)
+    public static void supplyFactoryDefaults(MappedConfiguration<String, String> conf) {
+        conf.add(MongoConfigurationSymbols.CONNECTION_HOST, "localhost");
+        conf.add(MongoConfigurationSymbols.CONNECTION_PORT, "27017");
+        conf.add(MongoConfigurationSymbols.AUTHENTICATION_DATABASE, "admin");
+        conf.add(MongoConfigurationSymbols.AUTHENTICATION_USERNAME, "");
+        conf.add(MongoConfigurationSymbols.AUTHENTICATION_PASSWORD, "");
+    }
+
     public static void bind(ServiceBinder binder) {
-        binder.bind(MongoService.class, MongoServiceImplementation.class).withId("asdas");
+        binder.bind(MongoService.class, MongoServiceImplementation.class);
     }
 }
