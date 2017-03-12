72dca0bffefac5c5af2631740a53b5bea5102363
Benjamin Weidig <ben@netzgut.net>
2017-03-12 14:44:35 +0100

    More MongoPersister methods

--- src/main/java/net/netzgut/integral/mongo/internal/services/MongoPersisterImplementation.java
+++ src/main/java/net/netzgut/integral/mongo/internal/services/MongoPersisterImplementation.java
@@ -3,8 +3,12 @@ package net.netzgut.integral.mongo.internal.services;
 import java.io.Serializable;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.UpdateResult;
 
 import net.netzgut.integral.mongo.services.MongoConverter;
 import net.netzgut.integral.mongo.services.MongoPersister;
@@ -26,6 +30,23 @@ public class MongoPersisterImplementation implements MongoPersister {
         MongoCollection<Document> collection = this.mongo.getCollection(entityClass);
         Document document = this.converter.documentFrom(entity);
         collection.insertOne(document);
+    }
+
+    @Override
+    public <T extends Serializable> UpdateResult replace(Bson filter, T entity) {
+        Class<? extends Serializable> entityClass = entity.getClass();
+        MongoCollection<Document> collection = this.mongo.getCollection(entityClass);
+        Document replacement = this.converter.documentFrom(entity);
+        return collection.replaceOne(filter, replacement);
+    }
+
+    @Override
+    public <T extends Serializable> UpdateResult upsert(Bson filter, T entity) {
+        Class<? extends Serializable> entityClass = entity.getClass();
+        MongoCollection<Document> collection = this.mongo.getCollection(entityClass);
+        Document document = this.converter.documentFrom(entity);
+        Bson update = Filters.eq("$set", document);
+        return collection.updateOne(filter, update, new UpdateOptions().upsert(true));
     }
 
 }
--- src/main/java/net/netzgut/integral/mongo/services/MongoPersister.java
+++ src/main/java/net/netzgut/integral/mongo/services/MongoPersister.java
@@ -1,8 +1,22 @@
 package net.netzgut.integral.mongo.services;
 
 import java.io.Serializable;
+import java.util.List;
+
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.result.UpdateResult;
 
 public interface MongoPersister {
 
     <T extends Serializable> void persist(T entity);
+
+    default <T extends Serializable> void persist(List<T> entities) {
+        entities.forEach(this::persist);
+    }
+
+    <T extends Serializable> UpdateResult replace(Bson filter, T entity);
+
+    <T extends Serializable> UpdateResult upsert(Bson filter, T entity);
+
 }
